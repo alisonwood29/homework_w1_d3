b33a3881578bdd782f813dd683435e6552ac3cb2
Alison Wood <alisonwood29@gmail.com>
2017-11-23 09:58:54 +0000

    homework solutions

--- homework_w1_d3_arrays_hashes.rb
+++ homework_w1_d3_arrays_hashes.rb
@@ -8,32 +8,38 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 stops.push("Edinburgh Waverley")
-p stops
+# p stops
 # 2. Add `"Glasgow Queen St"` to the start of the array
 stops.unshift("Glasgow Queen St")
-p stops
+# p stops
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
 stops.insert(4, "Polmont")
-p stops
+# p stops
 # 4. Work out the index position of `"Linlithgow"`
-p stops.index("Linlithgow")
+#p stops.index("Linlithgow")
 # 5. Remove `"Livingston"` from the array using its name
 stops.delete("Livingston")
-p stops
+# p stops
 # 6. Delete `"Cumbernauld"` from the array by index
-stops.slice!(2)
-p stops
+# stops.slice!(2)
+stops.delete_at(2)
+# p stops
 # 7. How many stops there are in the array?
-p stops.length()
+# p stops.length()
+# p stops.count
+# p stops.size
 # 8. How many ways can we return `"Falkirk High"` from the array?
-p stops[2]
-p stops[-5]
+# p stops[2]
+# p stops[-5]
+# p stops.fetch(2)
+# p stops.slice(2)
+# p stops.at(2)
 # 9. Reverse the positions of the stops in the array
-p stops.reverse()
+# p stops.reverse()
 # 10. Print out all the stops using a for loop
-for stop_name in stops
-  p stop_name
-end
+# for stop_name in stops
+#   p stop_name
+# end
 
 
 ## B. Given the following data structure:
@@ -71,24 +77,24 @@ end
   }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-p users["Jonathan"][:twitter]
+# p users["Jonathan"][:twitter]
 # # 2. Get Erik's hometown
-p users["Erik"][:home_town]
+# p users["Erik"][:home_town]
 # # 3. Get the array of Erik's favourite numbers
-p users["Erik"][:favourite_numbers]
+# p users["Erik"][:favourite_numbers]
 # # 4. Get the type of Avril's pet Colin
-p users["Avril"][:pets]["colin"]
+# p users["Avril"][:pets]["colin"]
 # 5. Get the smallest of Erik's favourite numbers
-p users["Erik"][:favourite_numbers][0]
+# p users["Erik"][:favourite_numbers].min()
 # # 6. Add the number `7` to Erik's favourite numbers
 users["Erik"][:favourite_numbers].push(7)
-p users["Erik"]
+# p users["Erik"]
 # # 7. Change Erik's hometown to Edinburgh
 users["Erik"][:home_town] = "Edinburgh"
-p users["Erik"]
+# p users["Erik"]
 # 8. Add a pet dog to Erik called "Fluffy"
 users["Erik"][:pets]["fluffy"] = :dog
-p users["Erik"]
+# p users["Erik"]
 # 9. Add yourself to the users hash
 users["Alison"] = {
         :twitter => "alisonwood29",
@@ -99,7 +105,7 @@ users["Alison"] = {
           "sparkles" => :fish
         }
 }
-p users
+# p users
 
 # C. Given the following data structure:
 
@@ -123,14 +129,15 @@ united_kingdom = [
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 united_kingdom[1][:capital] = "Cardiff"
-p united_kingdom
+# p united_kingdom
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
-united_kingdom << {
+northern_ireland = {
       name: "Northern Ireland",
       population: 1811000,
       capital: "Belfast"
   }
-  p united_kingdom
+  united_kingdom.push(northern_ireland)
+  # p united_kingdom
 # 3. Use a loop to print the names of all the countries in the UK.
 for country_name in united_kingdom
   p country_name[:name]
